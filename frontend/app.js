3aa1dd3dabf30b171f23f85381a8b1368c815db1
Rhennan <reehsecundario@gmail.com>
2026-04-15 18:15:58 -0300

    Reformulação layout e estilos

--- frontend/app.js
+++ frontend/app.js
@@ -5,20 +5,20 @@ const card_tipos = document.getElementById("card1")
 
 const card_styles = {
     fogo: {
-        bg: "bg-red-500",
-        borda: "border-red-700"
+        bg: "bg-gradient-to-br from-red-500/80 to-red-700/80",
+        borda: "border-red-400/50"
     },
     agua: {
-        bg: "bg-blue-500",
-        borda: "border-blue-700"
+        bg: "bg-gradient-to-br from-blue-500/80 to-blue-700/80",
+        borda: "border-blue-400/50"
     },
     planta: {
-        bg: "bg-green-500",
-        borda: "border-green-700"
+        bg: "bg-gradient-to-br from-green-500/80 to-green-700/80",
+        borda: "border-green-400/50"
     },
     eletrico: {
-        bg: "bg-yellow-500",
-        borda: "border-yellow-700"
+        bg: "bg-gradient-to-br from-yellow-400/80 to-yellow-600/80",
+        borda: "border-yellow-300/50"
     }
 }
 
@@ -34,31 +34,36 @@ function render() {
         container.innerHTML += `
         <div class="poke-card shadow-lg p-4 ${tipo_style.bg} ${tipo_style.borda}">
 
-            <img src="${p.image}"
-            class="mt-6 w-full mb-10 ">
+            <div class="flex justify-center -mt-10 mb-4">
+                <img src="${p.image}" 
+                class="w-64 h-64 object-contain drop-shadow-xl hover:scale-110 transition-transform duration-300">
+            </div>
             
             <div class="flex justify-between items-start mb-4">
-                <span class="bg-blue-500/20 text-blue-400 text-xs font-bold px-2 py-1 rounded">
+                <span class=" bg-white/10 backdrop-blur rounded-md text-white-400 text-xs font-bold px-2 py-1">
                     HP ${p.vida}
+                    <div class="w-full h-3 bg-black/30 rounded mt-2">
+                    <div class="h-full bg-green-400 rounded" style="width: ${p.vida / 2}%"></div>
+                    </div>
                 </span>
-                <span class="text-slate-400 text-xm font-bold">${p.tipo}</span>
+                <span class="px-2 py-1 text-xs font-bold rounded-full bg-black/30 backdrop-blur">
+                ${p.tipo}</span>
             </div>
             
             <h3 class="text-2xl font-bold text-white mb-1">${p.nome_pokemon}</h3>
             
             <div class="space-y-2 my-4">
-                <div class="text-sm">
-                    <span class="text-slate-500 uppercase text-[10px] font-bold block">Ataque Principal</span>
-                    <p class="text-slate-200">${p.ataque_principal}</p>
-                </div>
-                <div class="text-sm">
-                    <span class="text-slate-500 uppercase text-[10px] font-bold block">Ataque Secundário</span>
-                    <p class="text-slate-200">${p.ataque_secundario}</p>
+            <div class="p-3 text-black bg-white/10 backdrop-blur rounded-2xl">
+                <div class="flex justify-between items-center">
+                    <span class="font-bold">${p.ataque_principal}</span>
+                    <span class="font-bold">120</span>
                 </div>
             </div>
-
-            <div class="border-t border-slate-700 pt-3 mt-3 flex justify-between items-center text-xs">
-                Fraqueza: ${p.fraqueza}
+            <div class="p-3 text-black bg-white/10 backdrop-blur rounded-2xl">
+                <div class="flex justify-between items-center">
+                    <span class="font-bold">${p.ataque_secundario}</span>
+                    <span class="font-bold">120</span>
+                </div>
             </div>
         </div>
         `;
--- frontend/data/teste_chars.js
+++ frontend/data/teste_chars.js
@@ -36,8 +36,56 @@ export const pokemon = [
     "ataque_secundario": "Investida Trovão",
     "fraqueza": "Terra",
     "vida": 60,
+    "image": "assets/pikachu.png",
     "card_type": "eletrico"
+
   },
+
+    {
+    "nome_pokemon": "Charizard",
+    "tipo": "Fogo / Voador",
+    "ataque_principal": "Lança-Chamas",
+    "ataque_secundario": "Asa de Ferro",
+    "fraqueza": "Água / Pedra",
+    "vida": 180,
+    "image": "assets/charizard GEN9.png",
+    "card_type": "fogo"
+  },
+  
+  {
+    "nome_pokemon": "Blastoise",
+    "tipo": "Água",
+    "ataque_principal": "Hidrocanhão",
+    "ataque_secundario": "Quebra-Crânio",
+    "fraqueza": "Elétrico / Planta",
+    "vida": 200,
+    "image": "assets/blastoise GEN9.png",
+    "card_type": "agua"
+  },
+  {
+    "nome_pokemon": "Venusaur",
+    "tipo": "Planta / Venenoso",
+    "ataque_principal": "Chicote de Vinha",
+    "ataque_secundario": "Raio Solar",
+    "fraqueza": "Fogo / Gelo",
+    "vida": 190,
+    "image": "assets/venusaur GEN9.png",
+    "card_type": "planta"
+  },
+
+  {
+    "nome_pokemon": "Pikachu",
+    "tipo": "Elétrico",
+    "ataque_principal": "Choque do Trovão",
+    "ataque_secundario": "Investida Trovão",
+    "fraqueza": "Terra",
+    "vida": 60,
+    "image": "assets/pikachu GEN9.png",
+    "card_type": "eletrico"
+
+  },
+
+
   {
     "nome_pokemon": "Gengar",
     "tipo": "Fantasma / Venenoso",
