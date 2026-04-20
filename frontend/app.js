4cd84aff7f9e6d7a599e60a6d5e744240a00a7b4
Rhennan <reehsecundario@gmail.com>
2026-04-20 16:40:53 -0300

    reformulação de estilos

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,8 @@
 import { pokemon } from "./data/teste_chars.js";
 
 const container = document.getElementById("pokemon-list");
-const card_tipos = document.getElementById("card1")
+const homeSection = document.getElementById("home_page")
+const mainContent = document.querySelector("main")
 
 const card_styles = {
     fogo: {
@@ -19,78 +20,55 @@ const card_styles = {
     eletrico: {
         bg: "bg-gradient-to-br from-yellow-400/80 to-yellow-600/80",
         borda: "border-yellow-300/50"
-    }
+    },
 }
 
 
 function render_cards() {
 
-    container.innerHTML = "";
+    container.innerHTML = ""
 
     pokemon.forEach((p) => {
 
-        const tipo_style = card_styles[p.card_type]
+        const style = card_styles[p.card_type] || { bg: "bg-slate-800/70", borda: "border-slate-700" }
 
         container.innerHTML += `
-        <div class="poke-card shadow-lg p-4 ${tipo_style.bg} ${tipo_style.borda}">
-
-            <div class="flex justify-center -mt-10 mb-4">
-                <img src="${p.image}" 
-                class="w-64 h-64 object-contain drop-shadow-xl hover:scale-110 transition-transform duration-300">
-            </div>
-            
-            <div class="flex justify-between items-start mb-4">
-                <span class=" bg-white/10 backdrop-blur rounded-md text-white-400 text-xs font-bold px-2 py-1">
-                    HP ${p.vida}
-                    <div class="w-full h-3 bg-black/30 rounded mt-2">
-                    <div class="h-full bg-green-400 rounded" style="width: ${p.vida / 2}%"></div>
+            <div class="poke-card ${style.bg} ${style.borda}">
+                <div class="flex justify-center -mt-12 mb-4">
+                    <a href=${p.db_url}> 
+                    <img src="${p.sprit_url}" class="w-40 h-40 object-contain ">
+                    </a>
+                </div>
+                <div class="text-center">
+                    <h3 class="text-xl font-bold text-white uppercase tracking-tighter">${p.nome_pokemon}</h3>
+                    <span class="text-xs font-bold px-2 py-1 rounded bg-black/20">${p.tipo}</span>
+                </div>
+                <div class="mt-4 space-y-2">
+                    <div class="flex justify-between text-xs font-bold">
+                        <span>HP</span>
+                        <span>${p.vida}</span>
                     </div>
-                </span>
-                <span class="px-2 py-1 text-xs font-bold rounded-full bg-black/30 backdrop-blur">
-                ${p.tipo}</span>
-            </div>
-            
-            <h3 class="text-2xl font-bold text-white mb-1">${p.nome_pokemon}</h3>
-            
-            <div class="space-y-2 my-4">
-            <div class="p-3 text-black bg-white/10 backdrop-blur rounded-2xl">
-                <div class="flex justify-between items-center">
-                    <span class="font-bold">${p.ataque_principal}</span>
-                    <span class="font-bold">120</span>
+                    <div class="w-full h-1.5 bg-black/20 rounded-full overflow-hidden">
+                        <div class="h-full bg-green-400" style="width: ${p.vida / 2}%"></div>
+                    </div>
                 </div>
             </div>
-            <div class="p-3 text-black bg-white/10 backdrop-blur rounded-2xl">
-                <div class="flex justify-between items-center">
-                    <span class="font-bold">${p.ataque_secundario}</span>
-                    <span class="font-bold">120</span>
-                </div>
-            </div>
-        </div>
         `
-    });
+    })
 }
 
-
-let sttPage = "home"
-
 function nav(page){
-    sttPage = page
-
-    if (page === "home"){
-        render_home()
+     if (page === "home"){
+        homeSection.classList.remove("hidden")
+        mainContent.classList.add("hidden")
     }
 
     if (page === "dex"){
-        render_dex()
+        homeSection.classList.add("hidden")
+        mainContent.classList.remove("hidden")
+        render_cards()
+        render_cards_pequenos()
     }
-}
-
-function render_home() {
-    container.innerHTML = ""
-}
-
-function render_dex(){
-    render_cards()
 }
 
 document.getElementById("btn_home").addEventListener("click", ()=>{
--- frontend/data/teste_chars.js
+++ frontend/data/teste_chars.js
@@ -1,96 +1,25 @@
 export const pokemon = [
-  {
-    "nome_pokemon": "Charizard",
-    "tipo": "Fogo / Voador",
-    "ataque_principal": "Lança-Chamas",
-    "ataque_secundario": "Asa de Ferro",
-    "fraqueza": "Água / Pedra",
-    "vida": 180,
-    "image": "assets/charizard.png",
-    "card_type": "fogo"
-  },
-  {
-    "nome_pokemon": "Blastoise",
-    "tipo": "Água",
-    "ataque_principal": "Hidrocanhão",
-    "ataque_secundario": "Quebra-Crânio",
-    "fraqueza": "Elétrico / Planta",
-    "vida": 200,
-    "image": "assets/blastoise.png",
-    "card_type": "agua"
-  },
-  {
-    "nome_pokemon": "Venusaur",
-    "tipo": "Planta / Venenoso",
-    "ataque_principal": "Chicote de Vinha",
-    "ataque_secundario": "Raio Solar",
-    "fraqueza": "Fogo / Gelo",
-    "vida": 190,
-    "image": "assets/venusaur.png",
-    "card_type": "planta"
-  },
-  {
-    "nome_pokemon": "Pikachu",
-    "tipo": "Elétrico",
-    "ataque_principal": "Choque do Trovão",
-    "ataque_secundario": "Investida Trovão",
-    "fraqueza": "Terra",
-    "vida": 60,
-    "image": "assets/pikachu.png",
-    "card_type": "eletrico"
-
-  },
-
-    {
-    "nome_pokemon": "Charizard",
-    "tipo": "Fogo / Voador",
-    "ataque_principal": "Lança-Chamas",
-    "ataque_secundario": "Asa de Ferro",
-    "fraqueza": "Água / Pedra",
-    "vida": 180,
-    "image": "assets/charizard GEN9.png",
-    "card_type": "fogo"
-  },
   
-  {
-    "nome_pokemon": "Blastoise",
-    "tipo": "Água",
-    "ataque_principal": "Hidrocanhão",
-    "ataque_secundario": "Quebra-Crânio",
-    "fraqueza": "Elétrico / Planta",
-    "vida": 200,
-    "image": "assets/blastoise GEN9.png",
-    "card_type": "agua"
-  },
-  {
-    "nome_pokemon": "Venusaur",
-    "tipo": "Planta / Venenoso",
-    "ataque_principal": "Chicote de Vinha",
-    "ataque_secundario": "Raio Solar",
-    "fraqueza": "Fogo / Gelo",
-    "vida": 190,
-    "image": "assets/venusaur GEN9.png",
-    "card_type": "planta"
-  },
-
-  {
-    "nome_pokemon": "Pikachu",
-    "tipo": "Elétrico",
-    "ataque_principal": "Choque do Trovão",
-    "ataque_secundario": "Investida Trovão",
-    "fraqueza": "Terra",
-    "vida": 60,
-    "image": "assets/pikachu GEN9.png",
-    "card_type": "eletrico"
-
-  },
-
   {
     "nome_pokemon": "Gengar",
     "tipo": "Fantasma / Venenoso",
     "ataque_principal": "Bola Sombria",
     "ataque_secundario": "Comedor de Sonhos",
     "fraqueza": "Sombrio / Psíquico",
-    "vida": 130
+    "vida": 130,
+    "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/gengar.gif",
+    "db_url": "https://pokemondb.net/pokedex/gengar",
+    "card_type": "fantasma"
+  },
+    {
+    "nome_pokemon": "Misdreavus",
+    "tipo": "Fantasma",
+    "ataque_principal": "Bola Sombria",
+    "ataque_secundario": "Comedor de Sonhos",
+    "fraqueza": "Sombrio / Psíquico",
+    "vida": 80,
+    "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/misdreavus.gif",
+    "db_url": "https://pokemondb.net/pokedex/misdreavus",
+    "card_type": "fantasma"
   }
 ]
