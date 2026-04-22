7aa7a344fc6927f7c88e7a8d943ceb6868ecaa17
Rhennan <reehsecundario@gmail.com>
2026-04-21 23:06:33 -0300

    test

--- frontend/data/teste_chars.js
+++ frontend/data/teste_chars.js
@@ -21,5 +21,38 @@ export const pokemon = [
     "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/misdreavus.gif",
     "db_url": "https://pokemondb.net/pokedex/misdreavus",
     "card_type": "fantasma"
+  },
+  {
+    "nome_pokemon": "Umbreon",
+    "tipo": "Escuridão",
+    "ataque_principal": "Bola Sombria",
+    "ataque_secundario": "Comedor de Sonhos",
+    "fraqueza": "Sombrio / Psíquico",
+    "vida": 100,
+    "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/umbreon.gif",
+    "db_url": "https://pokemondb.net/pokedex/umbreon",
+    "card_type": "fantasma"
+  },
+  {
+    "nome_pokemon": "Darkrai",
+    "tipo": "Escuridão",
+    "ataque_principal": "Bola Sombria",
+    "ataque_secundario": "Comedor de Sonhos",
+    "fraqueza": "Sombrio / Psíquico",
+    "vida": 150,
+    "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/darkrai.gif",
+    "db_url": "https://pokemondb.net/pokedex/darkrai",
+    "card_type": "fantasma"
+  },
+    {
+    "nome_pokemon": "Darkrai",
+    "tipo": "Escuridão",
+    "ataque_principal": "Bola Sombria",
+    "ataque_secundario": "Comedor de Sonhos",
+    "fraqueza": "Sombrio / Psíquico",
+    "vida": 150,
+    "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/darkrai.gif",
+    "db_url": "https://pokemondb.net/pokedex/darkrai",
+    "card_type": "fantasma"
   }
 ]
