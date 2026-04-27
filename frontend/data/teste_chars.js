e555191aa2d04118c8796c08d8c47e1c85bec60f
Rhennan <reehsecundario@gmail.com>
2026-04-27 14:13:15 -0300

    test

--- frontend/data/teste_chars.js
+++ frontend/data/teste_chars.js
@@ -54,5 +54,16 @@ export const pokemon = [
     "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/darkrai.gif",
     "db_url": "https://pokemondb.net/pokedex/darkrai",
     "card_type": "fantasma"
+  },
+  {
+    "nome_pokemon": "Darkrai",
+    "tipo": "Escuridão",
+    "ataque_principal": "Bola Sombria",
+    "ataque_secundario": "Comedor de Sonhos",
+    "fraqueza": "Sombrio / Psíquico",
+    "vida": 150,
+    "sprit_url": "https://img.pokemondb.net/sprites/black-white/anim/normal/darkrai.gif",
+    "db_url": "https://pokemondb.net/pokedex/darkrai",
+    "card_type": "fantasma"
   }
 ]
